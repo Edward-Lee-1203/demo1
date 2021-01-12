9e6b3c8ea37bfa47f2a1178c26f9c8200007ddc8
Edward-Lee-1203 <thuanle321qn@gmail.com>
2021-01-12 17:44:00 +0700

    Revert "Revert "Revert "first add"""
    This reverts commit 21945eb0

--- src/main/java/com/dut/demo/repository/FacultyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dut.demo.repository;
-
-import com.dut.demo.model.Faculty;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacultyRepository extends CrudRepository<Faculty, Long> {
-}
